8a4b562aaf82f4ff11bee440463a88b3f626783d
Stephen <stephen@localhost.localdomain>
2024-12-09 16:20:42 -0500

    Correct variable name

--- mlpass/main.py
+++ mlpass/main.py
@@ -2,4 +2,4 @@ from getpass import getpass
 
 if __name__ == '__main__':
     print("Password strength checker")
-    enteredPassword = getpass("Enter a password (characters are hidden, press enter when done): ")
+    entered_password = getpass("Enter a password (characters are hidden, press enter when done): ")
