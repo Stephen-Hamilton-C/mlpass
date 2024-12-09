39504864b1e180edc7b948c8a4f2b027478d494e
Stephen <stephen@localhost.localdomain>
2024-12-09 11:29:34 -0500

    Init commit

---
+++ mlpass/main.py
@@ -0,0 +1,5 @@
+from getpass import getpass
+
+if __name__ == '__main__':
+    print("Password strength checker")
+    enteredPassword = getpass("Enter a password (characters are hidden, press enter when done): ")
